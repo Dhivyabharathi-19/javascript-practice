c353c96f8cc9e8e227cb1534860f1b4a91e47b45
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-08 17:21:22 +0530

    variable Declaration in javascript

---
+++ variable-Declaration/var.js
@@ -0,0 +1,6 @@
+var price = 250;
+var product = "teddy bear";
+var gst = 40;
+console.log("product: " + product);
+var total = gst + price;
+console.log("total: " + total);
---
+++ variable-Declaration/var2.js
@@ -0,0 +1,6 @@
+var fruitname = "apple";
+var count = 5;
+var price = 20;
+var total = count * price;
+console.log("fruitname:" + fruitname);
+console.log("total:" + total);
