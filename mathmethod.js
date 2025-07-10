cde5facbe34f225d3b38fb1b9669ca925d3877ad
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-10 18:20:08 +0530

    mathmethod in js

---
+++ mathmethod.js
@@ -0,0 +1,37 @@
+let num1 = 20;
+let num2 = 30;
+let num3 = 40;
+
+let result = Math.max(num1, num2, num3);
+let result2 = Math.min(num1, num2, num3);
+let result3 = Math.round(7.5);
+let result4 = Math.floor(7.5);
+let result5 = Math.ceil(7.5);
+let result6 = Math.trunc(-7);
+let result7 = Math.pow(22, 2);
+let result8 = Math.sqrt(25);
+let result9 = Math.cbrt(27);
+let result10 = Math.floor(Math.random() * 20) + 1;
+let result11 = Math.log10(100);
+let result12 = Math.PI.toFixed(2);
+let result13 = Math.sign(-5);
+let result14 = Math.log10(100);
+let result15 = Math.E;
+
+// Math.sign(-5) return (-1)or (5) return(1) or (0) return(0);
+
+console.log("result:" + result);
+console.log("result2:" + result2);
+console.log("result3:" + result3);
+console.log("result4:" + result4);
+console.log("result5:" + result5);
+console.log("result6:" + result6);
+console.log("result7:" + result7);
+console.log("result8:" + result8);
+console.log("result9:" + result9);
+console.log("result10:" + result10);
+console.log("result11:" + result11);
+console.log("result12:" + result12);
+console.log("result13:" + result13);
+console.log("result14:" + result14);
+console.log("result15:" + result15);
