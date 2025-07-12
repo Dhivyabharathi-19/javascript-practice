3bf17559d4d526fad24314ea75f8f70d603369f4
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-12 16:38:54 +0530

    declaration of literals in javascript

---
+++ literals.js
@@ -0,0 +1,6 @@
+let firstName = "dhivya";
+let lastName = "bharathi";
+let message = `hi, i am ${firstName} ${lastName} i am learning javascript`;
+console.log(message);
+
+// output:hi, i am dhivya bharathi i am learning javascript
