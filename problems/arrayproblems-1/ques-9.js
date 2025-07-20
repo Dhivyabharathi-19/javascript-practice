c898ba325b34aec6081c6ec8758318e03e1fe1f0
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-20 18:09:17 +0530

    problem solving

---
+++ problems/arrayproblems-1/ques-9.js
@@ -0,0 +1,12 @@
+// Reverse an array without modifying the original.
+
+let names = ["tom", "jerry", "dora"];
+
+let reversed = [...names].reverse();
+
+console.log(reversed);
+console.log(names);
+
+// output:
+// ['dora', 'jerry', 'tom' ]
+// [ 'tom', 'jerry', 'dora' ]
