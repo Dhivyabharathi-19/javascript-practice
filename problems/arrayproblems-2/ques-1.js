5f46de2a4e1b9a026f592cd2b8adf5ba6b147ce4
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-26 17:38:55 +0530

    Problem Solving

---
+++ problems/arrayproblems-2/ques-1.js
@@ -0,0 +1,13 @@
+// 1.Write a function to return the second largest number in an array.
+
+// let arr = [1, 2, 5, 6, 8, 9];
+
+let arr = [1, 2, 3, 5, 7, 7, 9];
+
+// let unique = arr.set
+
+let arrsort = arr.sort((a, b) => b - a);
+
+console.log(arr[1]);
+
+// output:7
