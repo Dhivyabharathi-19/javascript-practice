db3a7aa47beee004c8420c38485a79d9fda1c9a7
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-26 17:39:10 +0530

    Problem Solving

---
+++ problems/arrayproblems-2/ques-2.js
@@ -0,0 +1,12 @@
+// 2.Check if two arrays are equal.
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4, 5];
+
+let equal = true;
+
+if (arr1.length !== arr2.length) {
+  equal = false;
+}
+console.log(equal);
+
+// output: false;
