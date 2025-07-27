5da36efc159ea691f4530980b91e5b35b145c172
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-27 20:11:50 +0530

    problem solving

---
+++ problems/arrayproblems-2/ques-10.js
@@ -0,0 +1,9 @@
+// Write a function to insert a new element at a specific index in an array.
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.splice(2, 0, 55);
+
+console.log(numbers);
+
+// output:[ 1, 2, 55, 3, 4, 5 ]
