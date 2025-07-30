498ef8d3b498a085b4086c0ea229ae2aa67dbdb7
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-07-30 16:52:12 +0530

    Problem Solving

---
+++ problems/arrayproblems-2/ques-21.js
@@ -0,0 +1,9 @@
+// Give an array of strings, join them to form a  sentence ending with a period.
+
+let strings = ["I", "Learning", "Javascript"];
+
+let sentence = strings.join("  ");
+
+console.log(sentence + ".");
+
+// output:I  Learning  Javascript.
