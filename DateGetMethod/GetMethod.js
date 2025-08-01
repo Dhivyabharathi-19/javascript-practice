e69cbd6ff1440849cb6e734aabafc14205f0d9cd
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-01 18:42:48 +0530

    problem solving

---
+++ DateGetMethod/GetMethod.js
@@ -0,0 +1,10 @@
+const today = new Date();
+
+console.log("FullYear:" + today.getFullYear());
+console.log("Month:" + today.getMonth());
+console.log("Dater:" + today.getDate());
+console.log("Day:" + today.getDay());
+console.log("Hours:" + today.getHours());
+console.log("Minutes:" + today.getMinutes());
+console.log("Seconds:" + today.getSeconds());
+console.log("Milliseconds:" + today.getMilliseconds());
