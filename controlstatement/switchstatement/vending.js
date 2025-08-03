55b974c5f750305b7684f07de7d989775fb5dad5
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-03 17:28:20 +0530

    Problem Solving

---
+++ controlstatement/switchstatement/vending.js
@@ -0,0 +1,17 @@
+let a1 = 12;
+
+switch (a1) {
+  case 1:
+    console.log("you selected Chocolate");
+    break;
+
+  case 2:
+    console.log("you selected ice cream");
+    break;
+  case 12:
+    console.log("you selected Chips");
+    break;
+
+  default:
+    console.log("No one is selected");
+}
