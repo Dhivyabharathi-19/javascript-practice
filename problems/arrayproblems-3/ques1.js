b229355e9268da2ef1af3f56ebc46decabb1329f
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-06 17:00:45 +0530

    Problem Solving

---
+++ problems/arrayproblems-3/ques1.js
@@ -0,0 +1,8 @@
+// 1. Find the maximum and minimum element in an array.
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+max = Math.max(...a);
+min = Math.min(...a);
+console.log("Max:", max);
+console.log("Min:", min);
