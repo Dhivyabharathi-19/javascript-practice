007198bbf68827366ac0bd0b2540554d7e9a61a2
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-09 15:19:22 +0530

    Problem Solving

---
+++ problems/arrayproblems-3/ques16.js
@@ -0,0 +1,7 @@
+// Find the index of the first element greater than a given number.
+let arr = [2, 5, 8, 1, 10];
+let number = 6;
+
+let index = arr.findIndex((n) => n > number);
+
+console.log(index);
