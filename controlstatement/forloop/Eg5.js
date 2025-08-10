ba236a8c61e3ddebcce2374453826f177bd490d6
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-10 16:14:02 +0530

    Problem Solving

---
+++ controlstatement/forloop/Eg5.js
@@ -0,0 +1,9 @@
+let Shopping = ["bread", "chocolate", "Soap", "Chips"];
+
+for (i = 0; i < Shopping.length; i++) {
+  console.log("Item ", i + 1 + ":", Shopping[i]);
+}
+// output:Item  1: bread
+// Item  2: chocolate
+// Item  3: Soap
+// Item  4: Chips
