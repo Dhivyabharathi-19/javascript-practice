e07a28616b4d1b7824fb1773740aec7c5b32af38
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-11 15:56:53 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm4.js
@@ -0,0 +1,18 @@
+// Print each  Character in a word.
+
+let word = "JAVACRIPT";
+
+for (let i = 0; i < word.length; i++) {
+  console.log(word[i]);
+}
+
+// output:
+// J
+// A
+// V
+// A
+// C
+// R
+// I
+// P
+// T
