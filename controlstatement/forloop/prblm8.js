79877cca484b711b0c617a441df6c76ec290d9e6
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-13 16:02:55 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm8.js
@@ -0,0 +1,15 @@
+// Print characters from a string in reverse.
+
+let word = "dhivya";
+
+for (let i = word.length - 1; i >= 0; i--) {
+  console.log(word[i]);
+}
+
+// output:
+// a
+// y
+// v
+// i
+// h
+// d
