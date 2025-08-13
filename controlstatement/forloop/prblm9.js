f6a1249d29e7e093680acd0978faa01fe5ddec9d
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-13 16:03:06 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm9.js
@@ -0,0 +1,17 @@
+// Print only odd numbers between 1 and 5.
+
+for (let i = 1; i <= 15; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// output:
+// 1
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
