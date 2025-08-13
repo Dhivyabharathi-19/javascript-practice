ad68af567cba1880d3344f71ba3e133493827287
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-13 16:02:36 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm7.js
@@ -0,0 +1,17 @@
+// Print Squares of numbers from 1 to 10.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Square of ${i} is ${i * i}`);
+}
+
+// output:
+// Square of 1 is 1
+// Square of 2 is 4
+// Square of 3 is 9
+// Square of 4 is 16
+// Square of 5 is 25
+// Square of 6 is 36
+// Square of 7 is 49
+// Square of 8 is 64
+// Square of 9 is 81
+// Square of 10 is 100
