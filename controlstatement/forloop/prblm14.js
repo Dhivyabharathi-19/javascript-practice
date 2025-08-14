12c4aa608e17aa9c715783236f75eae4befcf4d6
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-14 17:18:44 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm14.js
@@ -0,0 +1,11 @@
+// Print reverse of a string
+
+let str = "apple";
+let reversed = " ";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  reversed += str[i];
+}
+console.log("Reversed :" + reversed);
+
+//output :Reversed : elppa
