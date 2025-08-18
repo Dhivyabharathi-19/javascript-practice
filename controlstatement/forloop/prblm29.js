0c7f458d91b541436638e41ac4769818ed9bb0fc
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-18 18:21:04 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm29.js
@@ -0,0 +1,16 @@
+// Display even numbers from an array.
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    console.log(arr[i]);
+  }
+}
+
+// output:
+// 2
+// 4
+// 6
+// 8
+// 10
