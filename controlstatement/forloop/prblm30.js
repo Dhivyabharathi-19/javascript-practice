7b54a76268e094adac3eba8a52675c209052a7aa
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-18 18:21:29 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm30.js
@@ -0,0 +1,16 @@
+// Print prime numbers between 1 and 50.
+
+for (let i = 2; i <= 50; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(i);
+  }
+}
