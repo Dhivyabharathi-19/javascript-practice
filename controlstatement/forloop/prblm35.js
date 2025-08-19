c08ad557fd494b85f58b59356eb49727505913fe
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-19 20:21:09 +0530

    Problem Solving

---
+++ controlstatement/forloop/prblm35.js
@@ -0,0 +1,13 @@
+// Count vowels in a string.
+let str = "PARROT";
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+  let ch = str[i].toLowerCase();
+
+  if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+    count++;
+  }
+}
+
+console.log("Total vowels:", count);
