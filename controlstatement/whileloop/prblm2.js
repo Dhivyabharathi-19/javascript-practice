dfdd7b0a4a2f07819281d764c2ddfec85e1e7766
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-25 11:21:36 +0530

    Problem Solving

---
+++ controlstatement/whileloop/prblm2.js
@@ -0,0 +1,20 @@
+// Print Character of a Word one by one.
+
+let word = "pineapple";
+let i = 0;
+
+while (i < word.length) {
+  console.log(word[i]);
+  i++;
+}
+
+// output:
+// p
+// i
+// n
+// e
+// a
+// p
+// p
+// l
+// e
