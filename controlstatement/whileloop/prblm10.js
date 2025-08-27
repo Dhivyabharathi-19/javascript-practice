457fc14556db1db2ae5198e5731d00d0a91b00d1
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-27 20:34:18 +0530

    Problem Solving

---
+++ controlstatement/whileloop/prblm10.js
@@ -0,0 +1,15 @@
+// Eat candies while u have some..
+
+let candies = 5;
+
+while (candies > 0) {
+  console.log("Yum! ate one candy");
+  candies--;
+}
+
+//Output:
+// Yum! ate one candy
+// Yum! ate one candy
+// Yum! ate one candy
+// Yum! ate one candy
+// Yum! ate one candy
