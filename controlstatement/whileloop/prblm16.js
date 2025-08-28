ac69e2a6e15a494ee4536e37ced04834d3ec2ba8
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-28 18:28:09 +0530

    Problem Solving

---
+++ controlstatement/whileloop/prblm16.js
@@ -0,0 +1,12 @@
+// Create an New Array With Doubled Values.
+
+let array = [1, 2, 3, 4, 5];
+
+let doubled = [];
+
+let i = 0;
+while (i < array.length) {
+  doubled.push(array[i] * 2);
+  i++;
+}
+console.log("Doubled  Array:" + doubled);
