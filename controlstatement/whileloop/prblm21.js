1c4ea0f3057e6c25e1242edce4beb7333e1ed3b2
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-29 09:03:00 +0530

    Problem Solving

---
+++ controlstatement/whileloop/prblm21.js
@@ -0,0 +1,18 @@
+// Write a JavaScript program that prints numbers from 50 down to 1 in steps of 5 using a while loop.
+let i = 50;
+while (i >= 1) {
+  console.log(i);
+  i -= 5;
+}
+
+// output:
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
