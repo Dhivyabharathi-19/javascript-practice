458f24451c1e04c8c3d2955e5a38f8694a4bb443
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-08-31 14:58:54 +0530

    Problem Solving

---
+++ set_and_set_method/arrayfrom.js
@@ -0,0 +1,9 @@
+// Convert a Set back into an array using Array.from().
+
+const vehiclesSet = new Set(["car", "bike", "bus"]);
+
+const vehiclesArray = Array.from(vehiclesSet);
+
+console.log(vehiclesArray);
+
+//output:[ 'car', 'bike', 'bus' ]
