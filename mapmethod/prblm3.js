22fc458f290bca3aa9da4f6537215b78e75e07fa
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-09-08 17:36:25 +0530

    Problem Solving

---
+++ mapmethod/prblm3.js
@@ -0,0 +1,11 @@
+// Check if a student name exists in the Map using .has().
+
+const studentMarks = new Map();
+
+studentMarks.set("Alice", 85);
+studentMarks.set("Bob", 92);
+studentMarks.set("Charlie", 78);
+
+console.log("Name Exists:", studentMarks.has("Alice"));
+
+//output:Name Exists: true
