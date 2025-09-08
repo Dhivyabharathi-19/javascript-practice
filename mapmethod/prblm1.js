6025744c8bd1d66177ba6b3b941220a3fa02083f
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-09-08 17:35:52 +0530

    Problem Solving

---
+++ mapmethod/prblm1.js
@@ -0,0 +1,11 @@
+// Create a Map to store student names and their marks. Add 3 entries.
+
+const studentMarks = new Map();
+
+studentMarks.set("Alice", 85);
+studentMarks.set("Bob", 92);
+studentMarks.set("Charlie", 78);
+
+console.log(studentMarks);
+
+// output:Map(3) { 'Alice' => 85, 'Bob' => 92, 'Charlie' => 78 }
