82f4bb3b2b23c3bb749f0419bdb540badbabe3ac
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-09-08 17:36:09 +0530

    Problem Solving

---
+++ mapmethod/prblm2.js
@@ -0,0 +1,11 @@
+// Use .get() to retrieve marks of a specific student from the Map.
+
+const studentMarks = new Map();
+
+studentMarks.set("Alice", 85);
+studentMarks.set("Bob", 92);
+studentMarks.set("Charlie", 78);
+
+console.log(studentMarks.get("Bob"));
+
+//output:92
