31ec6f2e0301cd1144fcdabd82b5d0561bde4894
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-09-09 14:27:32 +0530

    Problem Solving

---
+++ mapmethod/prblm5.js
@@ -0,0 +1,11 @@
+// Print the number of entries in the Map using .size.
+
+const studentMarks = new Map();
+
+studentMarks.set("Alice", 85);
+studentMarks.set("Bob", 92);
+studentMarks.set("Charlie", 78);
+
+console.log("Number of students:", studentMarks.size);
+
+// output:Number of students: 3
