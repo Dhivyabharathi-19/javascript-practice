ad23ca96a762199d43eec13469343b0dd2fdb0f9
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-11-02 16:17:13 +0530

    Even or Odd

---
+++ problem_solving/evenodd.js
@@ -0,0 +1,13 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let evencount = [];
+let oddcount = [];
+num.forEach((n) => {
+  if (n % 2 === 0) {
+    evencount.push(n);
+  } else {
+    oddcount.push(n);
+  }
+});
+console.log("oddcount::", oddcount);
+console.log("eventcount", evencount);
